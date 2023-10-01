f500bf626fded4160264435c0b8ee7f5530beac4
Luciano1323 <luhiebra@gmail.com>
2023-09-30 23:28:21 -0300

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,13 @@
+number = 5;
+const timer = setInterval(() => {
+    alert(number);
+    number--;
+    if(number == 0)  {
+     clearInterval(timer)
+     const IVA = 21
+     let precio = parseInt(prompt("Ingresar dinero"));
+     let emergente = alert(precio + IVA)
+     emergente
+     emergente = true  
+    }  
+}, 100);
