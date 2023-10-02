0b9376a128a3b109dea94a3c272753428c22e5bf
Luciano1323 <luhiebra@gmail.com>
2023-10-01 22:14:44 -0300

    Arreglo IVA y bucle agregado

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 number = 5;
 const timer = setInterval(() => {
-    alert(number);
-    number--;
-    if(number == 0)  {
-     clearInterval(timer)
-     const IVA = 21
-     let precio = parseInt(prompt("Ingresar dinero"));
-     let emergente = alert(precio + IVA)
-     emergente
-     emergente = true  
-    }  
-},);
+  alert(number);
+  number--;
+  if (number == 0) {
+    while (number == 0) {
+      number = 5;
+    }
+    let precio = parseInt(prompt("Ingresar dinero"));
+    const IVA = (precio * 21) / 100;
+    let emergente = alert(precio + IVA);
+    emergente;
+    emergente = true;
+  }
+});
