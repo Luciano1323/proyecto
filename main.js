85465839ea047c391108caddb7370163cb0fcdb1
Luciano1323 <luhiebra@gmail.com>
2023-10-02 22:07:28 -0300

    Nuevo codigo, mejor desarrollado

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-number = 5;
-const timer = setInterval(() => {
-  alert(number);
-  number--;
-  if (number == 0) {
-    while (number == 0) {
-      number = 5;
+let precio;
+
+while (true) {
+  let input = prompt("Ingrese el precio:");
+
+  if (input === null || input.trim() === "") {
+    alert("No ingresó un precio válido.");
+  } else {
+    precio = parseFloat(input);
+
+    if (!isNaN(precio)) {
+      break;
+    } else {
+      alert("Ingrese un precio válido.");
     }
-    let precio = parseInt(prompt("Ingresar dinero"));
-    const IVA = (precio * 21) / 100;
-    let emergente = alert(precio + IVA);
-    emergente;
-    emergente = true;
   }
-});
+}
+
+const tasaIVA = 0.21;
+
+const montoIVA = precio * tasaIVA;
+
+const precioTotal = precio + montoIVA;
+alert(
+  `Precio ingresado: $${precio.toFixed(2)}\nIVA (21%): $${montoIVA.toFixed(
+    2
+  )}\nPrecio Total: $${precioTotal.toFixed(2)}`
+);
