a1915a0f5559563a721a9a2f3cd371399d2287d7
Luciano1323 <luhiebra@gmail.com>
2023-10-02 22:08:47 -0300

    reorganizacion

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ let precio;
 
 while (true) {
   let input = prompt("Ingrese el precio:");
-
   if (input === null || input.trim() === "") {
     alert("No ingresó un precio válido.");
   } else {
     precio = parseFloat(input);
-
     if (!isNaN(precio)) {
       break;
     } else {
@@ -15,11 +13,8 @@ while (true) {
     }
   }
 }
-
 const tasaIVA = 0.21;
-
 const montoIVA = precio * tasaIVA;
-
 const precioTotal = precio + montoIVA;
 alert(
   `Precio ingresado: $${precio.toFixed(2)}\nIVA (21%): $${montoIVA.toFixed(
